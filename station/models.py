4a78f759d206d05fc992b6065c96243b1a077c21
unknown <r.priadko@ukr.net>
2025-11-12 11:30:59 +0200

    implement ticket and station models

--- station/models.py
+++ station/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=128)
+    latitude = models.FloatField()
+    longitude = models.CharField()
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField(default=0)
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        "Journey",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self):
+        return f"Ticket #{self.id} - Seat {self.seat} (Journey {self.journey_id})"
