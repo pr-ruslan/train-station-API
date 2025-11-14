8ac2c4e5e023fa8b1937e74d324fdba07482ddfc
unknown <r.priadko@ukr.net>
2025-11-14 15:00:38 +0200

    mplement ourney serializer

--- station/serializers.py
+++ station/serializers.py
@@ -44,8 +44,64 @@ class TrainListSerializer(TrainSerializer):
     )
 
 
-
 class TrainDetailSerializer(TrainSerializer):
     train_type = TrainTypeSerializer(many=True, read_only=True)
 
 
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance"
+        )
+
+
+class RouteListSerializer(RouteSerializer):
+    source = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="name"
+    )
+    destination = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="name"
+    )
+
+
+class RouteDetailSerializer(RouteListSerializer):
+    source = StationSerializer(read_only=True)
+    destination = StationSerializer(read_only=True)
+
+
+class JourneySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Journey
+        fields = (
+            "id",
+            "route",
+            "train",
+            "departure_time",
+            "arrival_time"
+        )
+
+
+class JourneyListSerializer(JourneySerializer):
+    route = RouteListSerializer(read_only=True)
+    train = TrainListSerializer(read_only=True)
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("id", "seat", "cargo", "order")
+
+
+class JourneyDetailSerializer(JourneySerializer):
+    route = RouteDetailSerializer(read_only=True)
+    train = TrainDetailSerializer(read_only=True)
+    tickets = TicketSerializer(
+        many=True,
+        read_only=True
+    )
