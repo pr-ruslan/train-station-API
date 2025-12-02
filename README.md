492b6c3a9143d0a017f694f3ebb51931c30d28fe
unknown <r.priadko@ukr.net>
2025-12-02 16:17:51 +0200

    add station models in to admin panel

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ API service for train station management written on DRF
 TODOs
 
 - implement admin panel
-- switch to postgree
+- tests
+- generate readme content
+- flake8
--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from station.models import (
+    Station,
+    TrainType,
+    Train,
+    Route,
+    Crew,
+    Journey,
+    Order,
+    Ticket,
+)
+
+admin.site.register(Station)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Order)
+admin.site.register(Ticket)
+admin.site.register(Journey)
